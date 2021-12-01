42baba5c2130d0982e8317b49915f7b2d258e1a1
Roberto Chasipanta <maniac787@gmail.com>
2021-11-30 21:56:09 -0500

    initial commit

---
+++ src/main/java/ec/com/nwi/utils/JasyptUI.java
@@ -0,0 +1,10 @@
+package ec.com.nwi.utils;
+
+import javax.swing.*;
+
+public class JasyptUI {
+    private JTextField txtUsuario;
+    private JTextField txtClave;
+    private JTextField txtUrl;
+    private JTextField txtLlave;
+}
